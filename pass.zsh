29b6156e620beaaffbce7ad303328b1cbf47feb4
mizu0141 <akira0910@icloud.com>
2023-05-07 07:58:19 +0900

    ファイル追加

---
+++ pass.zsh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# パスワードマネージャーへようこそ
+echo "パスワードマネージャーへようこそ！"
+
+# サービス名、ユーザー名、パスワードの入力を求める
+read -p "サービス名を入力してください: " service_name
+read -p "ユーザー名を入力してください: " username
+read -p "パスワードを入力してください: " password
+echo ""
+
+# 入力された情報をファイルに保存
+filename="${service_name}_${username}_${password}.txt"
+echo "サービス名: $service_name" > $filename
+echo "ユーザー名: $username" >> $filename
+echo "パスワード: $password" >> $filename
+
+# 完了のメッセージを表示
+echo "Thank you!"
