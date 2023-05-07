f569f87baac2939c61b7efd5d5fb3caa6f9f2979
mizu0141 <akira0910@icloud.com>
2023-05-07 15:59:37 +0900

    step1

--- pass.zsh
+++ pass.zsh
@@ -8,7 +8,7 @@ read -p "ユーザー名を入力してください: " username
 read -p "パスワードを入力してください: " password
 echo ""
 
-# 入力された情報をファイルに保存
+# 入力された情報をファイルに保存する
 filename="${service_name}_${username}_${password}.txt"
 echo "サービス名: $service_name" > $filename
 echo "ユーザー名: $username" >> $filename
